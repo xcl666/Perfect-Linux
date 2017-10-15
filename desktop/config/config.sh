c7d31a030926f5ca7af9c6f78078032f4251c6d5
Xue Chuangli <xcl@xcldesktop.xcldesktop.com>
2017-10-16 00:07:15 +0800

    i3 config

--- desktop/config/config.sh
+++ desktop/config/config.sh
@@ -2,3 +2,4 @@
 ln -s $HOME/Perfect-Linux/desktop/config/.vimrc $HOME/.vimrc
 ln -s $HOME/Perfect-Linux/desktop/config/.vim $HOME/.vim
 ln -s $HOME/Perfect-Linux/desktop/config/.zshrc $HOME/.zshrc
+ln -s $HOME/Perfect-Linux/desktop/config/i3config $HOME/.config/i3/config
