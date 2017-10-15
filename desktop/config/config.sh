f3afb6c3ea07eda4a6270728e7e52f77c394a931
Xue Chuangli <xcl@xcldesktop.xcldesktop.com>
2017-10-16 00:01:35 +0800

    add a shell to auto

---
+++ desktop/config/config.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+ln -s $HOME/Perfect-Linux/desktop/config/.vimrc $HOME/.vimrc
+ln -s $HOME/Perfect-Linux/desktop/config/.vim $HOME/.vim
+ln -s $HOME/Perfect-Linux/desktop/config/.zshrc $HOME/.zshrc
