173b20385931ee48e34190ebe22bcb980378aded
Xue Chuangli <xcl@xcldesktop.xcldesktop.com>
2017-10-21 19:59:14 +0800

    a shell for config

--- desktop/config/config.sh
+++ desktop/config/config.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-ln -s $HOME/Perfect-Linux/desktop/config/.vimrc $HOME/.vimrc
-ln -s $HOME/Perfect-Linux/desktop/config/.vim $HOME/.vim
-ln -s $HOME/Perfect-Linux/desktop/config/.zshrc $HOME/.zshrc
-ln -s $HOME/Perfect-Linux/desktop/config/i3config $HOME/.config/i3/config
+thepath=`pwd`
+echo $thepath
+ln -s $thepath/.vimrc $HOME/.vimrc
+ln -s $thepath/.vim $HOME/.vim
+ln -s $thepath/.zshrc $HOME/.zshrc
+ln -s $thepath/i3config $HOME/.config/i3/config
