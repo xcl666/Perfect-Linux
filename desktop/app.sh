344008db5d70d58068cda6d22ff1659eabe99b93
Xue Chuangli <xcl@xcldesktop.com>
2018-04-21 05:53:28 +0800

    add ohmyzsh

---
+++ desktop/app.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#get the best mirror
+#sudo cp ./config/sources.list /etc/apt/
+#install the app
+sudo apt install vim sudo tmux git wget zsh fcitx-googlepinyin w3m shadowsocks-libev command-not-found python3-pip fortune-mod aptitude axel mutt nfs-common lftp i3 rofi chromium tree exuberant-ctags rdesktop unar volumeicon-alsa vifm proxychains
